cdcb94a0226908af9559e545709ff7de73430c6d
pomaxime <maximepoyet330@gmail.com>
2026-09-04 15:47:49 +0200

    ADD -> dépendance pour npm

--- KeyBoard/KeyBoard.tsx
+++ KeyBoard/KeyBoard.tsx
@@ -1,4 +1,16 @@
-interface KeyBoard{
+interface KeyBoardProps {
     letters: string[];
+    onKeyPress: (letter: string) => void;
 }
 
+export function KeyBoard({letters, onKeyPress}: KeyBoardProps) {
+    return (
+        <div className="keyboard">
+            {letters.map((letter) => (
+                <button key={letter} className="key" type="button" onClick={() => onKeyPress(letter)}>
+                    {letter}
+                </button>
+            ))}
+        </div>
+    );
+}
