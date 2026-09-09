52dcc73b40d2a5d6c8dab11a83b67531fc5eceac
pomaxime <maximepoyet330@gmail.com>
2026-09-09 14:15:32 +0200

    FIX -> conflit

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles.css';
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
->>>>>>> main
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-<<<<<<< HEAD
-);
-=======
 )
->>>>>>> main
